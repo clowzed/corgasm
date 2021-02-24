63a4f8adc9d618e1de96c4f369ac6a528d7c109a
Dmitry Miasnenko <cl0wzed.exe@gmail.com>
2021-02-24 13:34:46 +0300

    init commit of configlib

---
+++ configlib/configlib.c
@@ -0,0 +1,233 @@
+#include "configlib.h"
+
+void corgasm_configlib_destroy_section_wrapped(void * self)
+{
+    if (self)
+        configlib.destroy_section((section *)self);
+}
+
+section * corgasm_configlib_new_section(const char * name)
+{
+    section * new_section = NULL;
+    if (name)
+    {
+        new_section = malloc(sizeof(section));
+        if (new_section)
+        {
+            new_section->name = stringlib.new_string_from(name);
+            new_section->data = dictlib.new_dict(300);
+        }
+        if (new_section && (!new_section->name || !new_section->data))
+            new_section = configlib.destroy_section(new_section);
+    }
+    return new_section;
+}
+
+section * corgasm_configlib_destroy_section(section * self)
+{
+    if (self)
+    {
+        //! Add destroy data
+        stringlib.destroy(self->name);
+        dictlib.destroy(self->data, stringlib.destroy_wrapped);
+        free(self);
+    }
+    return NULL;
+}
+
+#define info printf("state = %d c = %c\n", current_state, current_char);
+
+bool corgasm_configlib_parse(configparser *self, FILE *stream)
+{
+    short      current_state        = find_left_bracket;
+    bool       was_parsed           = true;
+    section *  current_section      = NULL;
+    string  *  current_section_name = stringlib.new_string();
+    string  *  current_key_name     = stringlib.new_string();
+    string  *  current_value        = stringlib.new_string();
+
+    if (self && stream)
+    {
+        while_reading(current_char, stream, current_state != error_occured)
+        {
+            info
+            switch (current_state)
+            {
+
+                case find_left_bracket:
+                {
+                    if (alphabet_contains(current_char, find_left_bracket))
+                    {
+                        if (current_char == '[')
+                        {
+                            stringlib.clear(current_section_name);
+                            current_state = reading_section_name;
+                        }
+                    }
+                    else
+                        current_state = error_occured;
+                break;
+                }
+
+
+
+                case reading_section_name:
+                {
+                    if (alphabet_contains(current_char, reading_section_name))
+                    {
+                        if (current_char == ']')
+                        {
+                            current_section = configlib.new_section(stringlib.extract(current_section_name));
+                            listlib.append(self->sections, (void *)current_section);
+                            stringlib.clear(current_key_name);
+                            stringlib.clear(current_value);
+                        }
+                        else if (current_char == '\n')
+                        {
+                            stringlib.clear(current_key_name);
+                            current_state = find_key_name;
+                        }
+                        else
+                            stringlib.add_char(current_section_name, current_char);
+                    }
+                    else
+                        current_state = error_occured;
+                break;
+                }
+
+                case find_key_name:
+                {
+                    if (alphabet_contains(current_char, find_key_name))
+                    {
+                        if (current_char == '[')
+                        {
+                            stringlib.clear(current_section_name);
+                            current_state = reading_section_name;
+                        }
+                        else if (current_char == splitter)
+                        {
+                            stringlib.clear(current_value);
+                            current_state = find_end_of_value;
+                        }
+                        else
+                            stringlib.add_char(current_key_name, current_char);
+                    }
+                    else
+                        current_state = error_occured;
+                    break;
+                }
+
+                case find_end_of_value:
+                {
+                    printf("7\n");
+                    printf("cont = %d\n", alphabet_contains(current_char, find_end_of_value));
+                    if (alphabet_contains(current_char, find_end_of_value))
+                    {
+                        if (current_char == '\n')
+                        {
+                            printf("1");
+                            dictlib.set(current_section->data,
+                                        stringlib.extract(current_key_name),
+                                        stringlib.new_string_from(stringlib.extract(current_value)));
+                            printf("2");
+                            stringlib.clear(current_value);
+                            stringlib.clear(current_key_name);
+                            printf("3");
+                            current_state = find_key_name;
+                        }
+                        else
+                        {
+                            printf("-\n");
+                            printf("current_value: %p\n", current_value);
+                            printf("data: %s\n", stringlib.extract(current_value));
+                            printf("current_value->length: %d\n", current_value->length);
+                            printf("current_char: %c\n", current_char);
+                            stringlib.add_char(current_value, current_char);
+                            printf("-\n");
+                        }
+                    }
+                    else
+                        current_state = error_occured;
+                    break;
+                }
+
+                case error_occured:
+                    was_parsed = false;
+            }
+        }
+    }
+    printf("Here!");
+    if (current_state != error_occured && current_value && current_value->length)
+        dictlib.set(current_section->data,
+                    stringlib.extract(current_key_name),
+                    stringlib.new_string_from(stringlib.extract(current_value)));
+    return was_parsed;
+}
+
+configparser * corgasm_configlib_new_configparser(const char * filename)
+{
+    configparser * new_configparser    = NULL;
+    bool           is_parsed_correctly = false;
+    FILE *         stream              = NULL;
+
+    if (filename)
+    {
+        new_configparser = malloc(sizeof(configparser));
+        if (new_configparser)
+        {
+            new_configparser->sections = listlib.new_list(corgasm_configlib_destroy_section_wrapped);
+            stream                     = fopen(filename, "r");
+            if (stream && new_configparser->sections)
+                is_parsed_correctly = corgasm_configlib_parse(new_configparser, stream);
+            if (!is_parsed_correctly)
+                new_configparser = configlib.destroy(new_configparser);
+            fclose(stream);
+        }
+    }
+    return new_configparser;
+}
+
+
+configparser * corgasm_configlib_destroy(configparser * self)
+{
+    if (self)
+    {
+        listlib.destroy(self->sections);
+        free(self);
+    }
+    return NULL;
+}
+
+void corgasm_configlib_represent(configparser * self)
+{
+    if (self)
+    {
+        foreach(section *, current_section, self->sections)
+        {
+            printf("section name: %s\n", stringlib.extract(current_section->name));
+            for (size_t i = 0; i < 300; i++)
+            {
+                void * data = current_section->data->data->data[i];
+                if (data) printf("value : %s\n", stringlib.extract((string *)data));
+            }
+        }
+    }
+}
+
+string * corgasm_configlib_get(uu const char * section_name, uu const char * key)
+{
+    return NULL;
+}
+
+#ifdef LIB_BUILD_CONFIGLIB
+
+int main()
+{
+    configparser * parser = configlib.new_configparser("./conf.ini");
+    printf("Parser: %p\n", parser);
+    configlib.represent(parser);
+    configlib.destroy(parser);
+    return 0;
+}
+
+#endif
